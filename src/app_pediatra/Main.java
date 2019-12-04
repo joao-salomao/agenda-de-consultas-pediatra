e7bb8b5429c78778238246c22a735a151ebf57e2
João Salomão <joaosalomao.613@gmail.com>
2019-12-03 21:54:53 -0300

    Criando função que insere valores padrões ao criar banco

--- src/app_pediatra/Main.java
+++ src/app_pediatra/Main.java
@@ -5,6 +5,10 @@
  */
 package app_pediatra;
 
+import controllers.SchedulesController;
+import java.util.ArrayList;
+import models.Schedule;
+import utils.Utils;
 import views.MainFrame;
 
 /**
@@ -18,6 +22,7 @@ public class Main {
      */
     public static void main(String[] args) {
         try {
+            insertDefaultValues();
             setLookAndFeel();
             new MainFrame().setVisible(true);
         } catch (Exception e) {
@@ -25,6 +30,35 @@ public class Main {
             System.exit(1);
         }
 
+    }
+
+    public static void insertDefaultValues() {
+        SchedulesController sc = new SchedulesController();
+        
+        if (sc.all().isEmpty()) {
+            Object[][] values = {
+                {"Ilha", 30, 2, "13:30:00", "09:00:00", "12:30:00", "18:00:00"},
+                {"Ilha", 30, 6, "13:30:00", "09:00:00", "12:30:00", "18:00:00"},
+                {"Tijuca", 30, 3, "13:30:00", "10:00:00", "12:30:00", "18:00:00"},
+                {"Tijuca", 30, 4, "13:30:00", "10:00:00", "12:30:00", "18:00:00"},
+                {"Bonsucesso", 30, 5, "13:30:00", "10:00:00", "12:30:00", "18:00:00"},};
+            
+            String format = "HH:mm:ss";
+            
+            for (int i = 0; i < 5; i++) {
+                Schedule s = new Schedule();
+                
+                s.setClinicName((String) values[i][0]);
+                s.setConsultationPeriod((int) values[i][1]);
+                s.setDayOfWeek((int) values[i][2]);
+                s.setInitialLunchTime(Utils.parseStringToDate((String) values[i][5], format));
+                s.setFinalLunchTime(Utils.parseStringToDate((String) values[i][3], format));
+                s.setFirstAppointmentTime(Utils.parseStringToDate((String) values[i][4], format));
+                s.setLastAppointmentTime(Utils.parseStringToDate((String) values[i][6], format));
+                
+                sc.store(s);
+            }
+        }
     }
 
     public static void setLookAndFeel() {
